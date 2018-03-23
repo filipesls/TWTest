85cbeadd51ff6cc6bf304d40b249f69d3dfe4721
Filipe Santana <filipe.santana@gmail.com>
2018-03-23 19:20:07 +0000

    including api and routes

---
+++ app/routes/index.js
@@ -0,0 +1,19 @@
+var api = require('../api'),
+    path = require('path');
+
+module.exports  = function(app) {
+    
+    app.route('/v1/contacts')
+        .post(api.adiciona)
+        .get(api.lista);
+
+    app.route('/v1/contacts/:contactId')
+        .delete(api.remove)
+        .get(api.busca)
+        .put(api.atualiza);
+
+    // habilitando HTML5MODE
+    app.all('/*', function(req, res) {
+        res.sendFile(path.resolve('public/index.html'));
+    });
+};
